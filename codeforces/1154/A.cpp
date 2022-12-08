b0387be8e60daf8b90180355148359926f2cfb4e
Satyam Modi <smodi50448@gmail.com>
2022-12-09 00:40:00 +0530

    Add solution for problem `A - Restoring Three Numbers` Link: https://codeforces.com/contest/1154/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1154/submission/184388418

---
+++ codeforces/1154/A.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define ll long long
+
+string strip(string s) {
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ' ') {
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+
+vector<string> split (const string &s, char delim) {
+	vector<string> result;
+	stringstream ss (s);
+	string item;
+
+	while (getline (ss, item, delim)) {
+        result.push_back (strip(item));
+    }
+
+    return result;
+}
+
+
+string join(vector<string> v, string delim) {
+	string out="";
+	for (int i = 0; i < v.size(); i++) {
+		if (i == v.size()-1) {
+			out.append(v[i]);
+		} else {
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done) {
+	if (done[n]) {
+		return isPrime[n];
+	}
+
+	if (n == 1) {
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2) {
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	} else {
+
+		for (ll i = 2; i <= sqrt(n); i++) {
+			if (n%i == 0) {
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+void solve() {
+	vector<int> v(4, 0);
+	for (int i = 0; i < 4; i++) {
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[3]-v[2] << " " << v[3]-v[1] << " " << v[3]-v[0] << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+}
