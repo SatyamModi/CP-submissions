004fc3b763b922db47607fd526ec2d8c1a4b6c6e
Satyam Modi <smodi50448@gmail.com>
2022-12-09 15:06:00 +0530

    Add solution for problem `A - Short Substrings` Link: https://codeforces.com/contest/1367/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1367/submission/184440378

---
+++ codeforces/1367/A.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define ll long long
+
+string strip(string s) {
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ' ') {
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+
+vector<string> split (const string &s, char delim) {
+	vector<string> result;
+	stringstream ss (s);
+	string item;
+
+	while (getline (ss, item, delim)) {
+        result.push_back (strip(item));
+    }
+
+    return result;
+}
+
+
+string join(vector<string> v, string delim) {
+	string out="";
+	for (int i = 0; i < v.size(); i++) {
+		if (i == v.size()-1) {
+			out.append(v[i]);
+		} else {
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done) {
+	if (done[n]) {
+		return isPrime[n];
+	}
+
+	if (n == 1) {
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2) {
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	} else {
+
+		for (ll i = 2; i <= sqrt(n); i++) {
+			if (n%i == 0) {
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+void solve() {
+
+	int t;
+	cin >> t;
+	while (t-- > 0) {
+
+		string b;
+		cin >> b;
+
+		int n = (b.length() + 2) / 2;
+
+		string s(n, 'a');
+		s[0] = b[0];
+		s[n-1] = b[b.length()-1];
+		for (int i = 1; i < n-1; i++) {
+			s[i] = b[2*i-1];
+		}
+		cout << s << "\n";
+	}
+
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+}
