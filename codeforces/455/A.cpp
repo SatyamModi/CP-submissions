c72d173d74b6ddf4384baa5ddaf2b235b434444c
Satyam Modi <smodi50448@gmail.com>
2022-12-10 23:19:00 +0530

    Add solution for problem `A - Boredom` Link: https://codeforces.com/contest/455/problem/A Tags: dp, *1500 Ref: https://codeforces.com/contest/455/submission/184609028

---
+++ codeforces/455/A.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define ll long long
+
+string strip(string s) {
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ' ') {
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+
+vector<string> split (const string &s, char delim) {
+	vector<string> result;
+	stringstream ss (s);
+	string item;
+
+	while (getline (ss, item, delim)) {
+        result.push_back (strip(item));
+    }
+
+    return result;
+}
+
+
+string join(vector<string> v, string delim) {
+	string out="";
+	for (int i = 0; i < v.size(); i++) {
+		if (i == v.size()-1) {
+			out.append(v[i]);
+		} else {
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done) {
+	if (done[n]) {
+		return isPrime[n];
+	}
+
+	if (n == 1) {
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2) {
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	} else {
+
+		for (ll i = 2; i <= sqrt(n); i++) {
+			if (n%i == 0) {
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+
+void solve() {
+
+	int n;
+	cin >> n;
+	vector<ll> a(1e5+1, 0);
+	int x;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		a[x] += x;
+	}
+
+	for (int i = 2; i < 1e5+1; i++) {
+		a[i] = max(a[i]+a[i-2], a[i-1]);
+	}
+
+	cout << a[1e5] << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+}
