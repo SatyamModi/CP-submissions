bc454f1aff1693aa4c12d7bf4300cb1ed52352cc
Satyam Modi <smodi50448@gmail.com>
2022-12-12 11:16:00 +0530

    Add solution for problem `A - Required Remainder` Link: https://codeforces.com/contest/1374/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1374/submission/184848468

---
+++ codeforces/1374/A.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define ll long long
+
+
+string strip(string s) {
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ' ') {
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+
+vector<string> split (const string &s, char delim) {
+	vector<string> result;
+	stringstream ss (s);
+	string item;
+
+	while (getline (ss, item, delim)) {
+        result.push_back (strip(item));
+    }
+
+    return result;
+}
+
+
+string join(vector<string> v, string delim) {
+	string out="";
+	for (int i = 0; i < v.size(); i++) {
+		if (i == v.size()-1) {
+			out.append(v[i]);
+		} else {
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done) {
+	if (done[n]) {
+		return isPrime[n];
+	}
+
+	if (n == 1) {
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2) {
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	} else {
+
+		for (ll i = 2; i <= sqrt(n); i++) {
+			if (n%i == 0) {
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+
+void solve() {
+
+	int t;
+	cin >> t;
+	while (t-- > 0) {
+
+		ll x, y, n;
+		cin >> x >> y >> n;
+
+		if (y + (n/x + 1)*x <= n) {
+			cout << y + (n/x + 1)*x << "\n";
+		}
+		else if (y + (n/x)*x <= n) {
+			cout << y + (n/x)*x  << "\n";
+		}  else {
+			cout << y + (n/x - 1)*x << "\n";
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+}
