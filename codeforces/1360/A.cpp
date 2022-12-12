0f036f45df1b3f0797df8112636c9c0ab3953e19
Satyam Modi <smodi50448@gmail.com>
2022-12-12 21:03:00 +0530

    Add solution for problem `A - Minimal Square` Link: https://codeforces.com/contest/1360/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1360/submission/184947027

---
+++ codeforces/1360/A.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define ll long long
+
+
+string strip(string s) {
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ' ') {
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+
+vector<string> split (const string &s, char delim) {
+	vector<string> result;
+	stringstream ss (s);
+	string item;
+
+	while (getline (ss, item, delim)) {
+        result.push_back (strip(item));
+    }
+
+    return result;
+}
+
+
+string join(vector<string> v, string delim) {
+	string out="";
+	for (int i = 0; i < v.size(); i++) {
+		if (i == v.size()-1) {
+			out.append(v[i]);
+		} else {
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done) {
+	if (done[n]) {
+		return isPrime[n];
+	}
+
+	if (n == 1) {
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2) {
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	} else {
+
+		for (ll i = 2; i <= sqrt(n); i++) {
+			if (n%i == 0) {
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+
+void solve() {
+
+	int t;
+	cin >> t;
+	
+	while (t-- > 0) {
+		int a, b;
+		cin >> a >> b;
+		if (a > b) {
+			if (b >= (float)a/2.0) {
+				cout << 4*b*b << "\n";
+			} else {
+				cout << a*a << "\n";
+			}
+		} else if (b > a) {
+			if (a >= (float)b/2.0) {
+				cout << 4*a*a << "\n";
+			} else {
+				cout << b*b << "\n";
+			}
+		} else {
+			cout << 4*a*a << "\n";
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+}
