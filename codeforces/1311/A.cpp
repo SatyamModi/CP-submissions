179908afffafddd2ef7c5b4a3e7c1af4fb0cadd9
Satyam Modi <smodi50448@gmail.com>
2022-12-14 21:35:00 +0530

    Add solution for problem `A - Add Odd or Subtract Even` Link: https://codeforces.com/contest/1311/problem/A Tags: greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1311/submission/185203013

---
+++ codeforces/1311/A.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+#define ll long long
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define input(t) cin >> t
+// #define push_back pb
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+string strip(string s) {
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != ' ') {
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+
+vector<string> split (const string &s, char delim) {
+	vector<string> result;
+	stringstream ss (s);
+	string item;
+
+	while (getline (ss, item, delim)) {
+        result.push_back(strip(item));
+    }
+
+    return result;
+}
+
+
+string join(vector<string> v, string delim) {
+	string out="";
+	for (int i = 0; i < v.size(); i++) {
+		if (i == v.size()-1) {
+			out.append(v[i]);
+		} else {
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done) {
+	if (done[n]) {
+		return isPrime[n];
+	}
+
+	if (n == 1) {
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2) {
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	} else {
+
+		for (ll i = 2; i <= sqrt(n); i++) {
+			if (n%i == 0) {
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+bool cmp(const pair<int,int>& a, const pair<int,int>& b) {
+	if (a.first < b.first) {
+		return true;
+	} else  {
+		return false;
+	}
+}
+
+void solve() {
+	int t;
+	cin >> t;
+
+	while (t-- > 0) {
+		int a, b;
+		cin >> a >> b;
+
+		if (a == b) {
+			cout << 0 << "\n";
+		} else if (a > b) {
+			if ((a - b)%2 == 0) {
+				cout << 1 << "\n";
+			} else {
+				cout << 2 << "\n";
+			}
+		} else {
+			if ((b-a)%2 == 1) {
+				cout << 1 << "\n";
+			} else {
+				cout << 2 << "\n";
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+}
