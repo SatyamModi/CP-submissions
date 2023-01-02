94c8a3ec51c8fa1f3110191dc6c7c21cee123816
Satyam Modi <smodi50448@gmail.com>
2023-01-02 17:32:00 +0530

    Add solution for problem `B - Anton and Digits` Link: https://codeforces.com/contest/734/problem/B Tags: brute force, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/734/submission/187610127

---
+++ codeforces/734/B.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+#define ll long long
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define input(t) cin >> t
+#define output(s) cout << s << "\n"
+// #define push_back pb
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+// vector<int> a(N, 0);
+string strip(string s)
+{
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] != ' ')
+		{
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+vector<string> split(const string &s, char delim)
+{
+	vector<string> result;
+	stringstream ss(s);
+	string item;
+
+	while (getline(ss, item, delim))
+	{
+		result.push_back(strip(item));
+	}
+
+	return result;
+}
+
+string join(vector<string> v, string delim)
+{
+	string out = "";
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (i == v.size() - 1)
+		{
+			out.append(v[i]);
+		}
+		else
+		{
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done)
+{
+	if (done[n])
+	{
+		return isPrime[n];
+	}
+
+	if (n == 1)
+	{
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2)
+	{
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	}
+	else
+	{
+
+		for (ll i = 2; i <= sqrt(n); i++)
+		{
+			if (n % i == 0)
+			{
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+bool cmp(const pair<int, int> &a, const pair<int, int> &b)
+{
+	if (a.first < b.first)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isSquare(int n)
+{
+	int x = (int)sqrt(n);
+	return (x * x == n);
+}
+
+void solve()
+{
+	int k2, k3, k5, k6;
+	cin >> k2 >> k3 >> k5 >> k6;
+	int num_256 = min(k2, min(k5, k6));
+	int num_32 = min(k3, k2 - num_256);
+	cout << 32*num_32 + 256*num_256 << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	solve();
+}
