a15c2b4cbaba714e0ce218167b358c2798c2015c
Satyam Modi <smodi50448@gmail.com>
2023-01-02 19:17:00 +0530

    Add solution for problem `A - Frog Jumping` Link: https://codeforces.com/contest/1077/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1077/submission/187620063

---
+++ codeforces/1077/A.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+#define ll long long
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define input(t) cin >> t
+#define output(s) cout << s << "\n"
+// #define push_back pb
+
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+// vector<int> a(N, 0);
+string strip(string s)
+{
+	int idx = 0;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] != ' ')
+		{
+			idx = i;
+			break;
+		}
+	}
+
+	s = s.substr(idx);
+
+	return s;
+}
+
+vector<string> split(const string &s, char delim)
+{
+	vector<string> result;
+	stringstream ss(s);
+	string item;
+
+	while (getline(ss, item, delim))
+	{
+		result.push_back(strip(item));
+	}
+
+	return result;
+}
+
+string join(vector<string> v, string delim)
+{
+	string out = "";
+	for (int i = 0; i < v.size(); i++)
+	{
+		if (i == v.size() - 1)
+		{
+			out.append(v[i]);
+		}
+		else
+		{
+			out.append(v[i]);
+			out.append(delim);
+		}
+	}
+	return out;
+}
+
+bool prime(ll n, vector<bool> &isPrime, vector<bool> &done)
+{
+	if (done[n])
+	{
+		return isPrime[n];
+	}
+
+	if (n == 1)
+	{
+		done[1] = true;
+		isPrime[1] = false;
+		return false;
+	}
+	if (n == 2)
+	{
+		done[2] = true;
+		isPrime[2] = true;
+		return true;
+	}
+	else
+	{
+
+		for (ll i = 2; i <= sqrt(n); i++)
+		{
+			if (n % i == 0)
+			{
+				done[n] = true;
+				isPrime[n] = false;
+				return false;
+			}
+		}
+		done[n] = true;
+		isPrime[n] = true;
+		return true;
+	}
+}
+
+bool cmp(const pair<int, int> &a, const pair<int, int> &b)
+{
+	if (a.first < b.first)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isSquare(int n)
+{
+	int x = (int)sqrt(n);
+	return (x * x == n);
+}
+
+int gcd( int a, int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+int lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+void solve()
+{
+	int t;
+	cin >> t;
+
+	while (t-- > 0) {
+		ll a, b, k;
+		cin >> a >> b >> k;
+
+		cout << (k/2)*(a-b) + (k%2) * a << "\n";
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	solve();
+}
